093786f1c8f376f1b96fed27fe981e4738b8e4df
Rodin Velichkov <rodin.velichkov@gmail.com>
2024-06-27 01:26:35 +0300

    [Fix Kubernetes and Helm providers]
    Details:
    - Added "--output", "json" parameters to the command fetching the token
      in providers.tf
    - The issue was due to the command returning output in table format
    - Explicitly setting the output format fixed the issue

--- terraform/infrastructure/providers.tf
+++ terraform/infrastructure/providers.tf
@@ -38,14 +38,12 @@ provider "aws" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  /*
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--output", "json"]
   }
-  */
+
 }
 
 # Helm provider configuration
@@ -54,12 +52,10 @@ provider "helm" {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.cluster.token
-    /*
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--output", "json"]
     }
-*/
   }
 }
